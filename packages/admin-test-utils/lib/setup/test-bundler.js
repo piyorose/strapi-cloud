5844ade0e1f7c4a7859532bdc01e7fa0b961963a
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-04-25 10:10:57 +0100

    chore: fix hp & upload tests

--- packages/admin-test-utils/lib/setup/test-bundler.js
+++ packages/admin-test-utils/lib/setup/test-bundler.js
@@ -1,5 +1,27 @@
 'use strict';
 
+/* eslint-disable no-undef */
+
 const noop = () => {};
-// eslint-disable-next-line no-undef
+
 Object.defineProperty(window, 'scrollTo', { value: noop, writable: true });
+
+/**
+ * JSDOM doesn't implement PointerEvent so we need to mock our own implementation
+ * Default to mouse left click interaction
+ * https://github.com/radix-ui/primitives/issues/1822
+ * https://github.com/jsdom/jsdom/pull/2666
+ */
+class MockPointerEvent extends Event {
+  constructor(type, props) {
+    super(type, props);
+    this.button = props.button || 0;
+    this.ctrlKey = props.ctrlKey || false;
+    this.pointerType = props.pointerType || 'mouse';
+  }
+}
+
+window.PointerEvent = MockPointerEvent;
+window.HTMLElement.prototype.scrollIntoView = jest.fn();
+window.HTMLElement.prototype.releasePointerCapture = jest.fn();
+window.HTMLElement.prototype.hasPointerCapture = jest.fn();
